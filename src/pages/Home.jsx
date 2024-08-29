036e8de74b39b0412647a07b00f42a54b736e65a
Nico <akebra77@gmail.com>
2024-08-29 15:39:26 +0200

    Added icons and responsiveness

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,11 @@ import { Link } from "react-router-dom";
 export default function Home () {
     return (
         <div className="flex h-screen flex-col items-center px-8 py-16 bg-gray-100">
-            <h1 className="text-5xl font-bold text-indigo-600 mb-6">
-                Create and view your To Do List 📝
+            <h1 className="text-5xl font-bold text-indigo-600 mb-6 text-center sm:text-4xl md:text-3xl">
+                Manage your To Do List 📝
             </h1>
-            <p className="text-lg text-gray-800 max-w-md text-center mb-8">
+             <p className="text-lg text-gray-800 max-w-md text-center mb-8 
+                sm:text-base md:text-lg  xl:text-2xl">
                 Create, edit, organize and save your To Do list here!
             </p>
             <Link to="/todo" className="bg-yellow-400 text-gray-900 px-6 py-3 rounded-full font-semibold shadow-lg hover:bg-yellow-500 transition">
