356defbc365f71c553ffedf69ce8351a1728931d
Nico <akebra77@gmail.com>
2024-08-29 15:38:25 +0200

    Added icons and adding responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,8 +5,7 @@ export default function Navbar () {
         <div className="flex justify-between gap-10 items-center bg-[#fbbd04b0] py-4 px-3 text-white">
             <Link to="/">
             <h4 className="font-mono font-bold text-3xl">
-                <i className="fa fa-sticky-note"></i> Todo
-                <i className="fa fa-shirt"></i>
+               <img src="/gubra_logo_white_banner.svg" alt="Gubra Logo" className="h-10" />
             </h4>
             </Link>
         </div>
